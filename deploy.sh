5483c1ebf9acf8106bf2ee281aec841ecb2f2e47
rosario VS <paradisekiss-yaoi@hotmail.com>
2020-03-30 14:00:47 -0600

    deployando

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<ChayoVS>/<aiGifs>.git master:gh-pages
+git push -f git@github.com:<ChayoVS>/<aiGifs>.git master:master
 
 cd -
